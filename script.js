a2c953596c4863f951028271625ff94940f4bfc9
GlebKuznetsov94 <101658832+GlebKuznetsov94@users.noreply.github.com>
2022-10-26 09:14:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// ASCII был первым набором символов (стандартом кодирования), используемым между компьютерами в Интернете.
+// Оба стандарта ISO-8859-1 (по умолчанию в HTML 4.01) и UTF-8 (по умолчанию в HTML5) построены на ASCII.
+// Для использования:
+// https://www.w3schools.com/charsets/ref_html_ascii.asp
+
+const resultElement = document.getElementById("result");
+const lengthElement = document.getElementById("length");
+const uppercaseElement = document.getElementById("uppercase");
+const lowercaseElement = document.getElementById("lowercase");
+const numbersElement = document.getElementById("numbers");
+const symbolsElement = document.getElementById("symbols");
+const generateElement = document.getElementById("generate");
+const clipboardElement = document.getElementById("clipboard");
+
+const getRandomLower = () =>
+  String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+const getRandomUpper = () =>
+  String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+
+  const getRandomNumber = () => {
+    const numbers = "1234567890";
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  };
+
+const getRandomSymbol = () => {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const randomFunctions = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+const createNotification = (message) => {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = message;
+  document.body.appendChild(notif);
+  setTimeout(() => notif.remove(), 3000);
+};
+
+clipboardElement.addEventListener("click", () => {
+  const password = resultElement.innerText;
+  if (!password) return;
+  const textarea = document.createElement("textarea");
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  createNotification("Пароль скопирован в буфер обмена");
+});
+
+generateElement.addEventListener("click", () => {
+  const length = +lengthElement.value;
+  const hasLower = lowercaseElement.checked;
+  const hasUpper = uppercaseElement.checked;
+  const hasNumber = numbersElement.checked;
+  const hasSymbol = symbolsElement.checked;
+  resultElement.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+const generatePassword = (lower, upper, number, symbol, length) => {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) return "";
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunctions[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+};
